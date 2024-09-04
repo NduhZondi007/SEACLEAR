58ff18cfddd3d7caa6ac1cc4e609fae0d0a0920f
mnqobinkabinde <nkabindemnqobi31@gmail.com>
2024-09-04 21:55:51 +0200

    Can navigate to the chat component

---
+++ frontend/src/GeneralChat.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+
+class GeneralChat extends React.Component {
+    state = {
+        details: [],
+    };
+    componentDidMount() {
+    }
+
+    render() {
+        return(
+            <div>
+            </div>
+        )
+    }
+}
+
+export default GeneralChat;
