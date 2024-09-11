fbcfc0f5836fc7c056b765a8a3a7b7008e15ff7c
Ndumiso Zondi <ndumisozondi007@gmail.com>
2024-09-11 02:45:54 +0200

    Implemented complete hero section with video

--- frontend/src/components/HomePage/HeroSection.js
+++ frontend/src/components/HomePage/HeroSection.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import './HeroSection.css';
-
+import './HeroSection.css'; // Import custom CSS if needed
 
 const HeroSection = () => {
     return (
         <header className="masthead">
-            {/* Video Background */}
-            <video className="background-video" autoPlay muted loop>
-                <source src="/videos/langebaan_video.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
-
-            {/* Hero section content */}
-            <div className="container px-4 px-lg-5 h-100">
-                <div className="text-center">
-                    <h1 > Real-Time Water Quality Monitoring</h1>
+            <div className="video-container">
+                <video id="HeroVideo" autoPlay muted loop>
+                    <source src="/videos/langebaan_video.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+            <div className="container">
+                <div className="">
+                    <h1 className="text-header">SEACLEAR - Real-Time Water Quality Monitoring</h1>
                     <hr className="divider" />
-                    <p className="mb-5">We help you stay informed about beach water quality for safer seaside activities.</p>
-
-                    <button className="btn-primary" onClick={() => window.location.href = '#about'}>Find Out More</button>
-                    
+                    <p className='hero-text'>We help you stay informed about beach water quality for safer seaside activities.</p>
+                    <button className="btn btn-primary btn-xl" onClick={() => window.location.href = '#about'}>Learn More</button>
                 </div>
             </div>
         </header>
