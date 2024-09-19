043b435bd050ed887d3da3d5dd6102e4169738f9
mnqobinkabinde <nkabindemnqobi31@gmail.com>
2024-09-19 20:10:45 +0200

    Fixed testing in the backend again

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,12 +94,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myproject',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
