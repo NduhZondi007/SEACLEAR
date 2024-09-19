452347bb979995803564ee2dde1735d6b98e4f89
mnqobinkabinde <nkabindemnqobi31@gmail.com>
2024-09-19 19:54:47 +0200

    Changed urls and backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,23 +7,23 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # API endpoints
-    path('api/beaches/', BeachView.as_view(), name='beach-list'),  # List all beaches or create a new beach
-    path('api/beaches/<int:pk>/', BeachView.as_view(), name='beach-detail'),  # Retrieve, update, or delete a specific beach by pk
+    path('beaches/', BeachView.as_view(), name='beach-list'),  # List all beaches or create a new beach
+    path('beaches/<int:pk>/', BeachView.as_view(), name='beach-detail'),  # Retrieve, update, or delete a specific beach by pk
 
-    path('api/weather/', WeatherView.as_view(), name='weather-list'),  # List all weather records or create a new weather record
+    path('weather/', WeatherView.as_view(), name='weather-list'),  # List all weather records or create a new weather record
 
-    path('api/water-quality/', WaterQualityView.as_view(), name='water-quality-list'),  # List all water quality records or create a new water quality record
+    path('water-quality/', WaterQualityView.as_view(), name='water-quality-list'),  # List all water quality records or create a new water quality record
 
-    path('api/reports/', CommunityReportView.as_view()),  # List all community reports or create a new report
-    path('api/reports/<int:pk>/', CommunityReportView.as_view()),  # Retrieve, update, or delete a specific community report by pk
+    path('reports/', CommunityReportView.as_view()),  # List all community reports or create a new report
+    path('reports/<int:pk>/', CommunityReportView.as_view()),  # Retrieve, update, or delete a specific community report by pk
 
 
     # Beach Specific Chat endpoints
-    path('api/beachSpecific-chat/', BeachSpecificChatView.as_view(), name='beach-specific-chat'),  # List all beach-specific chats or create a new chat
-    path('api/beachSpecific-chat/<int:pk>/like/', MessageView.as_view(), name='message'),  # List all beach-specific chats or create a new chat
+    path('beachSpecific-chat/', BeachSpecificChatView.as_view(), name='beach-specific-chat'),  # List all beach-specific chats or create a new chat
+    path('beachSpecific-chat/<int:pk>/like/', MessageView.as_view(), name='message'),  # List all beach-specific chats or create a new chat
 
     # Admin Profile endpoints
-    path('api/admin-profiles/', AdminProfileView.as_view(), name='admin-profiles'),  # List all admin profiles or create a new profile
-    path('api/adminLogin/', AdminLoginView.as_view(), name='admin-login'),
-    path('api/adminLogout/', AdminLogoutView.as_view(), name='admin-logout'),
+    path('admin-profiles/', AdminProfileView.as_view(), name='admin-profiles'),  # List all admin profiles or create a new profile
+    path('adminLogin/', AdminLoginView.as_view(), name='admin-login'),
+    path('adminLogout/', AdminLogoutView.as_view(), name='admin-logout'),
 ]
